7a1bf0d30937c080da28b75d9fbee1008ec4c860
tukkimiesjonni <tukkimiesjonni@gmail.com>
2024-11-18 20:00:34 +0200

    Added documentation for app.py

--- src/app.py
+++ src/app.py
@@ -6,15 +6,63 @@ from util import validate_reference
 
 @app.route("/")
 def index():
+
+    """
+    Render the index page.
+
+    ---
+
+    Fetches all references from the database using `get_references` and 
+    passes them to the "index.html" template for rendering.
+
+    ---
+
+    Returns:
+        Response: Rendered HTML page with a list of references.
+    """
+
     references = get_references()
     return render_template("index.html", references=references)
 
 @app.route("/new_reference")
 def new():
+    """
+    Render the page for creating a new reference.
+
+    ---
+
+    Renders the "new_reference.html" template where users can input details
+    for creating a new reference.
+
+    ---
+
+    Returns:
+        Response: Rendered HTML page for creating a new reference.
+    """
+     
     return render_template("new_reference.html")
 
 @app.route("/create_reference", methods=["POST"])
 def reference_creation():
+    """
+    Handle the creation of a new reference.
+
+    ---
+
+    Retrieves reference data from the request form, validates it, and 
+    creates a new reference in the database. Redirects to the home page on 
+    success or back to the new reference page on failure.
+
+    ---
+
+    Methods:
+        POST: Accepts data for the new reference from an HTML form.
+
+    Returns:
+        Response: Redirect to the home page ("/") on success, or to 
+                  "/new_reference" with an error message on failure.
+    """
+
     entry_type = request.form.get("entry_type")
     title = request.form.get("title")
     authors = request.form.get("authors")
@@ -30,11 +78,29 @@ def reference_creation():
 
 @app.route("/update_reference/<reference_id>", methods=["POST"])
 def update_reference(reference_id):
+    """
+    Not implemented yet.
+    """
+
+
     return redirect("/")
 
 # testausta varten oleva reitti
 if test_env:
     @app.route("/reset_db")
     def reset_database():
+        """
+        Reset the database (for testing purposes only).
+
+        ---
+
+        Clears the database by calling `reset_db` and returns a success 
+        message in JSON format. Only available in testing environments.
+
+        ---
+
+        Returns:
+            Response: JSON object with a success message.
+        """
         reset_db()
         return jsonify({ "message": "db reset" })
