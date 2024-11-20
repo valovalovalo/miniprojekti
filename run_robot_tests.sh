7837f78e0c738195256f4d11f8ec4d1f7465358f
tukkimiesjonni <tukkimiesjonni@gmail.com>
2024-11-20 09:31:36 +0200

    Modified the script so it runs from correct dir

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -20,7 +20,7 @@ done
 echo "Flask server is ready"
 
 # suoritetaan testit
-poetry run robot --variable HEADLESS:true src/story_tests
+poetry run robot --variable HEADLESS:true src/story_tests/
 
 status=$?
 
