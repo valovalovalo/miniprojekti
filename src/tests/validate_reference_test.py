338ae1a1f7d2dcca1307e355e9119244ce35b53a
Jasse <jasse.merivirta@helsinki.fi>
2024-11-20 12:12:53 +0200

    todo_testit muutettu reference testeiksi

---
+++ src/tests/validate_reference_test.py
@@ -0,0 +1,13 @@
+import unittest
+from util import validate_reference, UserInputError
+
+class TestReferenceValidation(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_valid_length_does_not_raise_error(self):
+        pass
+
+    def test_too_short_or_long_raises_error(self):
+        pass
+
