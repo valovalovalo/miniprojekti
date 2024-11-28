e5baaaf45def937206aa996f6321787d6448e207
Matti Averio <matti@averio.fi>
2024-11-28 02:51:33 +0200

    Referencen bibtext metodi

--- src/entities/reference.py
+++ src/entities/reference.py
@@ -5,6 +5,25 @@ class Reference:
         self.title = title
         self.authors = authors
         self.year = year
+        self.bibtext = self.get_bibtext()
+
+    def get_bibtext(self):
+        first_author = self.authors.split(",")[0].strip().split()[-1]
+        cite = f"{first_author.lower()}{self.year}"
+        
+        fields = {
+            'title': self.title,
+            'author': self.authors,
+            'year': self.year
+        }
+
+        bibtext = {
+            'type': self.entry_type,
+            'cite': cite,
+            'fields': fields
+        }
+
+        return bibtext
 
     def __str__(self):
         first_author = self.authors.split(",")[0].strip().split()[-1]
