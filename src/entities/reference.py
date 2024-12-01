0a9a042f012862ab9696b4c34455e929c1229cf4
Jasse <jasse.merivirta@helsinki.fi>
2024-12-01 12:31:45 +0200

    refaktorointia: Referencen kentät sanakirjaksi
    Reference-luokan kentät on nyt yksi sanakirja. Kenttiin pääsee käsiksi
    self.data.id, tai self.data.authors.

--- src/entities/reference.py
+++ src/entities/reference.py
@@ -1,38 +1,34 @@
 class Reference:
-    def __init__(self, id, entry_type, title, authors, year):
-        self.id = id
-        self.entry_type = entry_type
-        self.title = title
-        self.authors = authors
-        self.year = year
+    def __init__(self, data):
+        self.data = data
         self.bibtex = self.get_bibtex()
 
     def get_bibtex(self):
-        first_author = self.authors.split(",")[0].strip().split()[-1]
-        cite = f"{first_author.lower()}{self.year}"
+        first_author = self.data.authors.split(",")[0].strip().split()[-1]
+        cite = f"{first_author.lower()}{self.data.year}"
 
         fields = {
-            "title": self.title,
-            "author": self.authors,
-            "year": self.year
+            "title": self.data.title,
+            "author": self.data.authors,
+            "year": self.data.year
         }
 
         bibtex = {
-            "type": self.entry_type,
+            "type": self.data.entry_type,
             "cite": cite,
             "fields": fields
         }
 
         return bibtex
 
-    def __str__(self):
-        first_author = self.authors.split(",")[0].strip().split()[-1]
-        citation_key = f"{first_author.lower()}{self.year}"
+    # def __str__(self):
+    #     first_author = self.authors.split(",")[0].strip().split()[-1]
+    #     citation_key = f"{first_author.lower()}{self.year}"
 
-        return (
-            f"@{self.entry_type}{{{citation_key},\n"
-            f"  title = {{{self.title}}},\n"
-            f"  author = {{{self.authors}}},\n"
-            f"  year = {{{self.year}}}\n"
-            "}"
-        )
+    #     return (
+    #         f"@{self.entry_type}{{{citation_key},\n"
+    #         f"  title = {{{self.title}}},\n"
+    #         f"  author = {{{self.authors}}},\n"
+    #         f"  year = {{{self.year}}}\n"
+    #         "}"
+    #     )
