0cc4dd09879820ee12d3d3dbde73229cca6ea26d
tukkimiesjonni <tukkimiesjonni@gmail.com>
2024-12-02 17:54:06 +0200

    Commits for pull

--- src/repositories/reference_repository.py
+++ src/repositories/reference_repository.py
@@ -29,31 +29,56 @@ class ReferenceRepository:
         query = text(
             """
                 SELECT * FROM reference_entries WHERE reference_entries.id = (:reference_id)
-                """
+            """
         )
         query_result = self.db.session.execute(query, {"reference_id": reference_id})
         reference = query_result.mappings().first()
 
         return [Reference(reference)]
 
-    def create_reference(self, entry_type, title, authors, year):
-        sql = text(
-            """
-            INSERT INTO reference_entries (entry_type, title, authors, year) 
-            VALUES (:entry_type, :title, :authors, :year)
-        """
-        )
+    def create_reference(self, form_data):
 
-        self.db.session.execute(
-            sql,
-            {
-                "entry_type": entry_type,
-                "title": title,
-                "authors": authors,
-                "year": year,
-            },
-        )
+        test_data = {
+            "entry_type": "article",
+            "title": "abcdefg",
+            "authors": "hijklmn",
+            "year": "2000"
+        }
+        
+        columns = ", ".join(test_data.keys())
+        placeholders = ", ".join(f":{key}" for key in test_data.keys())
+
+        query = text(f"""
+            INSERT INTO reference_entries ({columns})
+            VALUES ({placeholders})
+        """)
+
+        parameters = {key: (value if value != "" else None) for key, value in test_data.items()}
+
+        self.db.session.execute(query, parameters)
         self.db.session.commit()
+
+
+
+
+
+  #      sql = text(
+  #          """
+  #          INSERT INTO reference_entries (entry_type, title, authors, year) 
+   #         VALUES (:entry_type, :title, :authors, :year)
+   #     """
+    #    )
+#
+ #       self.db.session.execute(
+  #          sql,
+   #         {
+    #            "entry_type": entry_type,
+     #           "title": title,
+      #          "authors": authors,
+       #         "year": year,
+       #     },
+       # )
+       # self.db.session.commit()
 
     def remove_reference(self, reference_id):
         sql = text(
